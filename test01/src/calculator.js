be7697879d81f48c768463057a8e0d4165c9c9c6
JHS5020 <goals00002@gmail.com>
2025-04-16 16:56:23 +0900

    04.15

--- test01/src/calculator.js
+++ test01/src/calculator.js
@@ -1,61 +1,40 @@
-for(let i = 1; i<=9; i+=2){
-    for(let j = 4; j <= 0; j--){
-        console.log(" ");
-    }
-    console.log("*");
-}
-let text00 = " ";
-let text11 = "*";
-    for(let i = 0; i < 5; i++){
-        let ww = text00.repeat(5 - i -1);
-        let qq = text11.repeat(2*i+1);
-        console.log(ww+qq);
-    }
-let lotto = [];
-for(let i = 0; i < 6; i++){
-    lotto.push(Math.floor(Math.random() * 45 + 1));
-}
-lotto.sort(function(a, b){return a - b});
-console.log(lotto);
+class joy{
+    start(){
+        let strInput2 = prompt("키를 입력하십시오");
+            if(strInput2 < 150){
+                console.log("No");
+            }
+            else if(strInput2 >= 150){
+                console.log("Yes");
+            }
+            else{
+                console.log("");
+            }
 
-let strInput = prompt("구하려는 번째의 숫자를 입력하세요 1 ~ 8");
-
-const planet = new Map([
-    ["1", "수성"],
-    ["2", "금성"],
-    ["3", "지구"],
-    ["4", "화성"],
-    ["5", "목성"],
-    ["6", "토성"],
-    ["7", "천왕성"],
-    ["8", "해왕성"],
-]);
-let numb = planet.get(strInput);
-console.log(numb);
-
-//거꾸로
-//로꾸꺼
-let strInput1 = prompt("문장을 입력하세요");
-let strr = "";
-for(let i = 1; i < strInput1.length + 1; i++){
-    strr += strInput1.charAt(strInput1.length - i);
-}
-console.log(strr);
-
-class Wizard{
-    health = "";
-    mana = "";
-    armor = "";
-
-    constructor(health, mana, armor){
-        this.health = health;
-        this.mana = mana;
-        this.armor = armor;
-    }
-    attack(){
-        console.log("파이어볼");
     }
 }
-const x = new Wizard(545,210,10);
-console.log(x.health, x.mana, x.armor);
-x.attack();
+
+class Num{
+    num1 = 0;
+    num2 = 0;
+    num3 = 0;
+
+    constructor(num1, num2, num3){
+        this.num1 = num1;
+        this.num2 = num2;
+        this.num3 = num3;
+    }
+    average(){
+        let d = (this.num1+this.num2+this.num3)/3;
+
+        console.log(Math.floor(d));
+    }
+}
+
+
+
+
+let Jo = new joy();
+Jo.start();
+let aver = new Num(20,35,46);
+aver.average();

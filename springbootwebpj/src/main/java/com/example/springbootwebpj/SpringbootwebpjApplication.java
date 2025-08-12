31164cf05f01d538dbb8195fe11ad53583936b00
JHS5020 <goals00002@naver.com>
2025-08-12 09:50:51 +0900

    08_12 springboot

---
+++ springbootwebpj/src/main/java/com/example/springbootwebpj/SpringbootwebpjApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootwebpj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootwebpjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootwebpjApplication.class, args);
+    }
+
+}
